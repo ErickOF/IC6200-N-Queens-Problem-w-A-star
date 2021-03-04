aab8175e6837136200cbdd8b2ffdae611cd8a05b
Jose Nunez <jose.nunez@advisiondevelopment.com>
2021-03-03 22:54:51 -0600

    [F]: add initial variables

---
+++ main.py
@@ -0,0 +1,14 @@
+import constants as _c
+
+matrix = _c.EMPTY_MATRIX
+
+open_list = _c.LIST_EMPTY
+closed_list = _c.LIST_EMPTY
+tentative_routes_list = _c.LIST_EMPTY
+
+print(matrix[0][0]['ID'])
+
+open_list.append(matrix[0][0])
+print(open_list)
+
+
